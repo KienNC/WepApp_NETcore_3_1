ac019cd7e22c40c7a64dd6e88bf2d9c68e0f8aba
Kiên Nguyễn <nguyencongkien.bk@outlook.com.vn>
2020-09-10 21:25:42 +0700

    Create soluiton stucture

---
+++ eShopSolution.Data/Configurations/ProductConfigration.cs
@@ -0,0 +1,26 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+   public class ProductConfigration: IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.DateCreated);
+            builder.Property(x => x.Price).IsRequired();
+
+            builder.Property(x => x.OriginalPrice).IsRequired();
+
+            builder.Property(x => x.Stock).IsRequired().HasDefaultValue(0);
+
+            builder.Property(x => x.ViewCount).IsRequired().HasDefaultValue(0);
+        }
+    }
+}
---
+++ eShopSolution.Data/EF/EShopSolutionContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopSolutionContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            // get connection string
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json")
+             .Build();
+            var ConnectionString = configuration.GetConnectionString("eShopSolutionDb");
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
